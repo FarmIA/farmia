43eec281ee164cf1dc2eb3ad8d4c52d5340587b0
Tamiir <paul@roqueta.fr>
2023-11-22 19:35:25 +0100

    création premiere sequence (fonctions virtuelles)

---
+++ pole hardware/Sequence1.lua
@@ -0,0 +1,42 @@
+-- Attention il faudra remplacer les fonctions par celles correspondantes du FarmBot
+
+-- Coordonnées des piles de plantes
+local stacks = {
+    {x1, y1, z1},
+    {x2, y2, z2},
+    {x3, y3, z3},
+    {x4, y4, z4},
+    {x5, y5, z5},
+    {x6, y6, z6},
+    {x7, y7, z7}
+}
+
+-- Distance entre les plantes
+local distance = d
+
+-- Fonction waterPlant
+function waterPlant(x,y,z)
+    goTo(x,y,z)
+    water(HIGH)
+    sleep(T)
+    water(LOW)
+end
+
+-- Fonction pour arroser une pile de plantes
+function waterStack(stack)
+    for i=1, 3 do
+        -- Arrose la plante à la position actuelle
+        waterPlant(stack[1], stack[2] + i*distance, stack[3])
+    end
+    for i=1, 3 do
+        -- Arrose la plante à la position actuelle
+        waterPlant(stack[1] + distance, stack[2] + i*distance, stack[3])
+    end
+end
+
+-- Arrose toutes les piles de plantes
+for _, stack in ipairs(stacks) do
+    waterStack(stack)
+end
+
+goTo(0,0,0)
